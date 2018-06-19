bf3d3fabadadbdc64686874580320681bc92a7e3
darthjee <darthjee@gmail.com>
2018-06-19 21:04:04 +0200

    WIP

--- lib/azeroth/resourceable/routes_builder.rb
+++ lib/azeroth/resourceable/routes_builder.rb
@@ -10,15 +10,26 @@ module Azeroth::Resourceable
     end
 
     def append
-      %i(index show new edit create update).each do |route|
-        add_method(route, 'render_basic')
+      %i(index show new edit create update destroy).each do |route|
+        add_method(route, route_code[route].to_s)
       end
-      add_method(:destroy,
-        <<-RUBY
-          #{resource}.destroy
-          head :no_content
-        RUBY
-      )
+    end
+
+    private
+
+    def route_code
+      {
+        index: :render_basic,
+        show: :render_basic,
+        new: :render_basic,
+        edit: :render_basic,
+        create: :render_basic,
+        update: :render_basic,
+        destroy: <<-RUBY
+                   #{resource}.destroy
+                   head :no_content
+                 RUBY
+      }
     end
   end
 end
