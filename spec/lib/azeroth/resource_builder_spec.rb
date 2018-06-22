9aae7543250259b63966064dd27160106114b5a5
darthjee <darthjee@gmail.com>
2018-06-22 07:44:33 +0200

    Add specs for ResourceBuilder

--- spec/lib/azeroth/resource_builder_spec.rb
+++ spec/lib/azeroth/resource_builder_spec.rb
@@ -1,5 +1,47 @@
 require 'spec_helper'
 
 describe Azeroth::ResourceBuilder do
-  xit 'Need to write it'
+  subject { described_class.new(model, builder) }
+
+  let(:model)   { Azeroth::Model.new(:document) }
+  let(:builder) { Sinclair.new(klass) }
+  let(:klass)   { Class.new(ResourceBuilderController) }
+
+  before do
+    subject.append
+    10.times { Document.create }
+  end
+
+  describe '#append' do
+    it 'adds the listing method' do
+      expect { builder.build }
+        .to change { klass.new.respond_to?(:documents) }
+        .to(true)
+    end
+
+    it 'adds the fetching method' do
+      expect { builder.build }
+        .to change { klass.new.respond_to?(:document) }
+        .to(true)
+    end
+
+    context 'after the build' do
+      let(:controller) { klass.new(document_id: document.id) }
+      let(:document)   { Document.create }
+
+      before { builder.build }
+
+      context 'when requesting the list of documents' do
+        it 'returns the list of documents' do
+          expect(controller.documents).to eq(Document.all)
+        end
+      end
+
+      context 'when requesting one document' do
+        it 'returns the requested document' do
+          expect(controller.document).to eq(document)
+        end
+      end
+    end
+  end
 end
