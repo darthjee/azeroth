44add1a96d844bc56e4f390790474829589c8355
darthjee <darthjee@gmail.com>
2018-06-22 18:27:10 +0200

    Extract shared examples

--- spec/lib/azeroth/resourceable/builder_spec.rb
+++ spec/lib/azeroth/resourceable/builder_spec.rb
@@ -17,6 +17,68 @@ shared_examples 'a builder that adds a resource for route' do |route|
   end
 end
 
+shared_examples 'a route resource build' do
+  describe 'index_resource' do
+    it_behaves_like 'a builder that adds a resource for route', :index do
+      let(:expected) { Document.all }
+    end
+  end
+
+  describe 'new_resource' do
+    it_behaves_like 'a builder that adds a resource for route', :new do
+      let(:expected) { Document.new }
+    end
+  end
+
+  describe 'create_resource' do
+    it_behaves_like 'a builder that adds a resource for route', :create do
+      let(:expected) { Document.last }
+      let(:params) { { document: document_params } }
+
+      it 'creates the resource' do
+        subject.build
+        expect do
+          instance.create_resource
+        end.to change(Document, :count).by(1)
+      end
+    end
+  end
+
+  describe 'show_resource' do
+    let(:document) { Document.create }
+
+    it_behaves_like 'a builder that adds a resource for route', :show do
+      let(:expected) { document }
+      let(:params) { { id: document.id } }
+    end
+  end
+
+  describe 'update_resource' do
+    let!(:document) { Document.create }
+
+    it_behaves_like 'a builder that adds a resource for route', :update do
+      let(:expected) { Document.find(document.id) }
+      let(:params) { { id: document.id, document: document_params } }
+
+      context 'after the methods has been built' do
+        before { subject.build }
+
+        it 'updates the resource the resource' do
+          expect do
+            instance.update_resource
+          end.to change { document.reload.name }.to(name)
+        end
+
+        it 'does not create the resource' do
+          expect do
+            instance.update_resource
+          end.not_to change(Document, :count)
+        end
+      end
+    end
+  end
+end
+
 describe Azeroth::Resourceable::Builder do
   subject { described_class.new(clazz, :document) }
 
@@ -29,64 +91,6 @@ describe Azeroth::Resourceable::Builder do
   end
 
   describe '#build' do
-    describe 'index_resource' do
-      it_behaves_like 'a builder that adds a resource for route', :index do
-        let(:expected) { Document.all }
-      end
-    end
-
-    describe 'new_resource' do
-      it_behaves_like 'a builder that adds a resource for route', :new do
-        let(:expected) { Document.new }
-      end
-    end
-
-    describe 'create_resource' do
-      it_behaves_like 'a builder that adds a resource for route', :create do
-        let(:expected) { Document.last }
-        let(:params) { { document: document_params } }
-
-        it 'creates the resource' do
-          subject.build
-          expect do
-            instance.create_resource
-          end.to change(Document, :count).by(1)
-        end
-      end
-    end
-
-    describe 'show_resource' do
-      let(:document) { Document.create }
-
-      it_behaves_like 'a builder that adds a resource for route', :show do
-        let(:expected) { document }
-        let(:params) { { id: document.id } }
-      end
-    end
-
-    describe 'update_resource' do
-      let!(:document) { Document.create }
-
-      it_behaves_like 'a builder that adds a resource for route', :update do
-        let(:expected) { Document.find(document.id) }
-        let(:params) { { id: document.id, document: document_params } }
-
-        context 'after the methods has been built' do
-          before { subject.build }
-
-          it 'updates the resource the resource' do
-            expect do
-              instance.update_resource
-            end.to change { document.reload.name }.to(name)
-          end
-
-          it 'does not create the resource' do
-            expect do
-              instance.update_resource
-            end.not_to change(Document, :count)
-          end
-        end
-      end
-    end
+    it_behaves_like 'a route resource build'
   end
 end
