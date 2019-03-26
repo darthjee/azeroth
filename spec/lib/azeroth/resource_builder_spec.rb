23a4ac65e38be0883714de037634df267bc8809d
darthjee <darthjee@gmail.com>
2019-03-26 18:24:42 +0100

    Fix subject usage

--- spec/lib/azeroth/resource_builder_spec.rb
+++ spec/lib/azeroth/resource_builder_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Azeroth::ResourceBuilder do
-  subject { described_class.new(model, builder) }
+  subject(:resource_builder) { described_class.new(model, builder) }
 
   let(:model)   { Azeroth::Model.new(:document) }
   let(:builder) { Sinclair.new(klass) }
   let(:klass)   { Class.new(ResourceBuilderController) }
 
   before do
-    subject.append
+    resource_builder.append
     10.times { Document.create }
   end
 
--- spec/lib/azeroth/resource_route_builder_spec.rb
+++ spec/lib/azeroth/resource_route_builder_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
 describe Azeroth::ResourceRouteBuilder do
-  subject { described_class.new(model, builder) }
+  subject(:resource_routes_builder) do
+    described_class.new(model, builder)
+  end
 
   let(:model)   { Azeroth::Model.new(:document) }
   let(:builder) { Sinclair.new(klass) }
   let(:klass)   { Class.new(ResourceRouteBuilderController) }
 
   before do
-    subject.append
+    resource_routes_builder.append
     10.times { Document.create }
   end
 
