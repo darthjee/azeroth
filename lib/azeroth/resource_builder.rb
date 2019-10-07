7fce2109f4324d472e326b95b08afa22e7a15e46
darthjee <darthjee@gmail.com>
2019-10-07 21:24:06 +0200

    Add builder docs

--- lib/azeroth/resource_builder.rb
+++ lib/azeroth/resource_builder.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 module Azeroth
+  # @api private
+  # @author Darthjee
+  #
+  # Builder responsible for for building the resource methods
+  #
+  # The builder adds 2 methods, one for listing all
+  # entries of a resource, and one for fetching an specific
+  # entry
   class ResourceBuilder
     attr_reader :model, :builder
+    # @method model
+    # @api private
+    #
+    # Returns the model class of the resource
+    #
+    # @return [Model]
 
     delegate :add_method, to: :builder
     delegate :name, :plural, to: :model
