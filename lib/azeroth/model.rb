ebb846fa10f9192940d6d56ca1e468566cdb45fe
darthjee <darthjee@gmail.com>
2019-10-07 20:11:23 +0200

    Add documentation to Model

--- lib/azeroth/model.rb
+++ lib/azeroth/model.rb
@@ -1,17 +1,34 @@
 # frozen_string_literal: true
 
 module Azeroth
+  # @api private
+  # @author Darthjee
+  #
+  # Model responsible for making the conection to the resource model class
   class Model
     attr_reader :name
+    # @api private
+    # @method name
+    #
+    # Returns the name of the resource represented by the model
+    #
+    # @return [String]
 
+    # @param name [String,Symbol] name of the resource
     def initialize(name)
       @name = name.to_s
     end
 
+    # resource class (real model class)
+    #
+    # @return [Class]
     def klass
       @klass ||= name.camelize.constantize
     end
 
+    # Return the pluralized version of resource name
+    #
+    # @return [String]
     def plural
       name.pluralize
     end
