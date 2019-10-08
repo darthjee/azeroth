24538d51fc8b43560256b29f08c5cbbf5b9fb1d5
darthjee <darthjee@gmail.com>
2019-10-08 09:46:25 +0200

    Improve builder docs

--- lib/azeroth/resource_builder.rb
+++ lib/azeroth/resource_builder.rb
@@ -10,25 +10,46 @@ module Azeroth
   # entries of a resource, and one for fetching an specific
   # entry
   class ResourceBuilder
-    attr_reader :model, :builder
-    # @method model
-    # @api private
-    #
-    # Returns the model class of the resource
-    #
-    # @return [Model]
-
     delegate :add_method, to: :builder
     delegate :name, :plural, to: :model
 
+    # @param model [Model] Resource model interface
+    # @param builder [Sinclair] method builder
     def initialize(model, builder)
       @model = model
       @builder = builder
     end
 
+    # Add methods to be built
+    #
+    # Methods are the listing of all entities and fetching
+    # of an specific entity
+    #
+    # @return [Array<Sinclair::MethodDefinition>]
     def append
       add_method(plural, "@#{plural} ||= #{model.klass}.all")
       add_method(name, "@#{name} ||= #{plural}.find(#{name}_id)")
     end
+
+    private
+
+    attr_reader :model, :builder
+    # @method model
+    # @api private
+    # @private
+    #
+    # Returns the model class of the resource
+    #
+    # @return [Model]
+
+    # @method builder
+    # @api private
+    # @private
+    #
+    # Returns a method builder
+    #
+    # @return [Sinclair]
+    #
+    # @see https://www.rubydoc.info/gems/sinclair Sinclair
   end
 end
