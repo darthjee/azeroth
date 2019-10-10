ddd4cd7292f4356eba795e221344264aa9ec4414
darthjee <darthjee@gmail.com>
2019-10-10 21:03:22 +0200

    Add more documentation

--- lib/azeroth/resourceable.rb
+++ lib/azeroth/resourceable.rb
@@ -3,12 +3,20 @@
 require 'active_support'
 
 module Azeroth
+  # @api public
+  # @author Darthjee
+  #
+  # Concern for building controller methods for the routes
   module Resourceable
     extend ActiveSupport::Concern
 
     autoload :Builder, 'azeroth/resourceable/builder'
 
     class_methods do
+      # Adds resource methods for resource
+      #
+      # @param name [String, Symbol] Name of the resource
+      # @param options [Hash]
       def resource_for(name, **options)
         Builder.new(
           self, name, Azeroth::Options.new(options)
