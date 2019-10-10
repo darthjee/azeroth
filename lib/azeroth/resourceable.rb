eeb3c3d69ca061fa3ec0a8ee20320cce8223e0e5
darthjee <darthjee@gmail.com>
2019-10-10 22:41:23 +0200

    Add resourceable documentation

--- lib/azeroth/resourceable.rb
+++ lib/azeroth/resourceable.rb
@@ -11,17 +11,6 @@ module Azeroth
     extend ActiveSupport::Concern
 
     autoload :Builder, 'azeroth/resourceable/builder'
-
-    class_methods do
-      # Adds resource methods for resource
-      #
-      # @param name [String, Symbol] Name of the resource
-      # @param options [Hash]
-      def resource_for(name, **options)
-        Builder.new(
-          self, name, Azeroth::Options.new(options)
-        ).build
-      end
-    end
+    autoload :ClassMethods, 'azeroth/resourceable/class_methods'
   end
 end
---
+++ lib/azeroth/resourceable/class_methods.rb
@@ -0,0 +1,19 @@
+module Azeroth
+  module Resourceable
+    # @api public
+    # @author Darthjee
+    #
+    # Class methods added by {Resourceable}
+    module ClassMethods
+      # Adds resource methods for resource
+      #
+      # @param name [String, Symbol] Name of the resource
+      # @param options [Hash]
+      def resource_for(name, **options)
+        Builder.new(
+          self, name, Azeroth::Options.new(options)
+        ).build
+      end
+    end
+  end
+end
