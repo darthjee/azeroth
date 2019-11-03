553e99ea360520a10a779a39e89dd6e9b6da5b0c
darthjee <darthjee@gmail.com>
2019-11-03 10:55:40 +0100

    Fix controller spec

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -15,20 +15,34 @@ describe DocumentsController do
       end
     end
 
-    before do
-      get :index, params: { format: :json }
-    end
-
-    it { expect(response).to be_successful }
-
-    it { expect(parsed_response).to eq([]) }
-
-    context 'when there is a document' do
-      let(:documents_count) { 1 }
+    context 'when calling on format json' do
+      before do
+        get :index, params: { format: :json }
+      end
 
       it { expect(response).to be_successful }
 
-      it { expect(parsed_response).to eq(documents.as_json) }
+      it { expect(parsed_response).to eq([]) }
+
+      context 'when there is a document' do
+        let(:documents_count) { 1 }
+
+        it { expect(response).to be_successful }
+
+        it "renders documents json" do
+          expect(parsed_response).to eq(documents.as_json)
+        end
+      end
+    end
+
+    context 'when calling on format html' do
+      before do
+        get :index, params: { format: :html }
+      end
+
+      it { expect(response).to be_successful }
+
+      it { expect(response).to render_template("documents/index") }
     end
   end
 end
