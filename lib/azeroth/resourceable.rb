6c484ea504e178ed8954bd07658e4aa845d1d1cc
darthjee <darthjee@gmail.com>
2019-11-03 10:58:27 +0100

    Move render_basic to resourceable

--- lib/azeroth/resourceable.rb
+++ lib/azeroth/resourceable.rb
@@ -12,5 +12,15 @@ module Azeroth
 
     autoload :Builder, 'azeroth/resourceable/builder'
     autoload :ClassMethods, 'azeroth/resourceable/class_methods'
+
+    private
+
+    def render_basic
+      action = params[:action]
+      respond_to do |format|
+        format.json { render json: send("#{action}_resource") }
+        format.html { action }
+      end
+    end
   end
 end
--- spec/dummy/app/controllers/documents_controller.rb
+++ spec/dummy/app/controllers/documents_controller.rb
@@ -4,14 +4,4 @@ class DocumentsController < ApplicationController
   include Azeroth::Resourceable
 
   resource_for :document
-
-  private
-
-  def render_basic
-    action = params[:action]
-    respond_to do |format|
-      format.json { render json: send("#{action}_resource") }
-      format.html { action }
-    end
-  end
 end
