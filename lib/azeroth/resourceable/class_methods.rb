253749cee9c650eee781b17daf08d30db8b15c20
darthjee <darthjee@gmail.com>
2019-11-12 16:03:05 +0100

    Improove docs

--- lib/azeroth/resourceable/class_methods.rb
+++ lib/azeroth/resourceable/class_methods.rb
@@ -11,6 +11,15 @@ module Azeroth
       #
       # @param name [String, Symbol] Name of the resource
       # @param options [Hash]
+      #
+      # @return [Array<MethodDefinition>]
+      #
+      # @example
+      #   class DocumentsController < ApplicationController
+      #     include Azeroth::Resourceable
+      #
+      #     resource_for :document
+      #   end
       def resource_for(name, **options)
         Builder.new(
           self, name, Azeroth::Options.new(options)
