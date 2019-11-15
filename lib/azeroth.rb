ae3bc13d794089d798e8ebbbcde470f8b52de599
darthjee <darthjee@gmail.com>
2019-11-15 23:04:03 +0100

    Remove ResourceRouteBuilder

--- lib/azeroth.rb
+++ lib/azeroth.rb
@@ -15,7 +15,6 @@ module Azeroth
   autoload :RequestHandler,       'azeroth/request_handler'
   autoload :Resourceable,         'azeroth/resourceable'
   autoload :ResourceBuilder,      'azeroth/resource_builder'
-  autoload :ResourceRouteBuilder, 'azeroth/resource_route_builder'
   autoload :RoutesBuilder,        'azeroth/routes_builder'
   autoload :Options,              'azeroth/options'
 end
