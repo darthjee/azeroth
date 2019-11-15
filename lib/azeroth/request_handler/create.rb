842152a4088cee2a0eda6edde8225345ad4b62a5
darthjee <darthjee@gmail.com>
2019-11-15 11:57:55 +0100

    Add RequestHandler::Create

---
+++ lib/azeroth/request_handler/create.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'arstotzka'
+
+module Azeroth
+  class RequestHandler
+    class Create < RequestHandler
+      private
+
+      def resource
+        attributes = controller.send("#{model.name}_params")
+        collection = controller.send(model.plural)
+        collection.create(attributes)
+      end
+    end
+  end
+end
