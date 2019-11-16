63d3f150d0832a3154ea463d443d555cd283951c
darthjee <darthjee@gmail.com>
2019-11-16 18:59:07 +0100

    Add Destroy documentation

--- lib/azeroth/request_handler/destroy.rb
+++ lib/azeroth/request_handler/destroy.rb
@@ -2,9 +2,17 @@
 
 module Azeroth
   class RequestHandler
+    # @api private
+    #
+    # hadler for requests to destroy resources
     class Destroy < RequestHandler
       private
 
+      # @private
+      #
+      # Destroy and return an instance of the model
+      #
+      # @return [Object]
       def resource
         resource = controller.send(model.name)
         resource.tap(&:destroy)
