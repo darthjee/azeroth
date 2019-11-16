43a1c142eaf68021dbe2eaeb6dff6b3e7add57f1
darthjee <darthjee@gmail.com>
2019-11-16 19:01:11 +0100

    Add documentation for Edit

--- lib/azeroth/request_handler/edit.rb
+++ lib/azeroth/request_handler/edit.rb
@@ -2,6 +2,12 @@
 
 module Azeroth
   class RequestHandler
+    # @api private
+    #
+    # hadler for requests to edit resources
+    #
+    # This handler does the same as {Show}
+    # returning the model for editing
     class Edit < RequestHandler::Show
     end
   end
