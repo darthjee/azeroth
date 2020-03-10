5a0ca6f51a5b906255dae5c693fc5cc8c05c3c62
darthjee <darthjee@gmail.com>
2020-03-10 10:18:08 +0100

    Rubocop fix

--- spec/support/factories/document.rb
+++ spec/support/factories/document.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :document, class: ::Document do
+  factory :document, class: '::Document' do
     sequence(:name) { |n| "Name-#{n}" }
   end
 end
--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user, class: ::User do
+  factory :user, class: '::User' do
     sequence(:name)  { |n| "Name-#{n}" }
     sequence(:email) { |n| "user-#{n}@email.com" }
   end
