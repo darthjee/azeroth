bc658fde59cb9b73381bd589689830b88128e6fb
darthjee <darthjee@gmail.com>
2020-04-24 01:25:06 +0200

    Add options#actions

--- lib/azeroth/options.rb
+++ lib/azeroth/options.rb
@@ -3,10 +3,16 @@
 module Azeroth
   class Options < ::OpenStruct
     DEFAULT_OPTIONS = {
+      only: %i[create destroy edit index new show update],
+      except: []
     }.freeze
 
-    def initialize(options)
+    def initialize(options = {})
       super(DEFAULT_OPTIONS.merge(options))
+    end
+
+    def actions
+      only.map(&:to_sym) - except.map(&:to_sym)
     end
   end
 end
---
+++ spec/lib/azeroth/options_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Azeroth::Options do
+  subject(:options) { described_class.new }
+
+  describe "#actions" do
+    it 'returns default actions' do
+      expect(options.actions)
+        .to eq(%i[create destroy edit index new show update])
+    end
+
+    context 'when passing only options' do
+      subject(:options) { described_class.new(only: [:index]) }
+
+      it 'returns defined actions' do
+        expect(options.actions)
+          .to eq(%i[index])
+      end
+    end
+
+    context 'when passing except options' do
+      subject(:options) do
+        described_class.new(except: [:index, :create])
+      end
+
+      it 'returns not excluded actions' do
+        expect(options.actions)
+          .to eq(%i[destroy edit new show update])
+      end
+    end
+  end
+end
