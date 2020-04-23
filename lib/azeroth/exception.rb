446fc055bebcee2639a1dabe16fb5f2951dffcd9
darthjee <darthjee@gmail.com>
2020-04-24 01:56:57 +0200

    Add yard documentation

--- lib/azeroth/exception.rb
+++ lib/azeroth/exception.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 module Azeroth
+  # @api public
+  # @author Darthjee
+  #
+  # Excaptions raised by azeroth
   class Exception < ::StandardError
+    # @api public
+    # @author Darthjee
+    #
+    # Exception raised when invalid options are given
     class InvalidOptions < Azeroth::Exception
+      # @param invalid_keys [Array<Symbol>] list of invalid keys
       def initialize(invalid_keys = [])
         @invalid_keys = invalid_keys
       end
 
+      # Exception string showing invalid keys
+      #
+      # @return [String]
       def message
         keys = invalid_keys.join(' ')
         "Invalid keys on options initialization (#{keys})"
@@ -15,6 +27,13 @@ module Azeroth
       private
 
       attr_reader :invalid_keys
+      # @method invalid_keys
+      # @api private
+      # @private
+      #
+      # invalid keys on options initialization
+      #
+      # @return [Array<Symbol>]
     end
   end
 end
