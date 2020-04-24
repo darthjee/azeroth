d557719f3ecdc32713b25640c893f0d1a88f3163
darthjee <darthjee@gmail.com>
2020-04-24 17:10:36 +0200

    Add dummy decorator

--- lib/azeroth.rb
+++ lib/azeroth.rb
@@ -10,12 +10,13 @@ require 'sinclair'
 #
 # @see Resourceable
 module Azeroth
-  autoload :Decorator,            'azeroth/decorator'
-  autoload :Exception,            'azeroth/exception'
-  autoload :Model,                'azeroth/model'
-  autoload :RequestHandler,       'azeroth/request_handler'
-  autoload :Resourceable,         'azeroth/resourceable'
-  autoload :ResourceBuilder,      'azeroth/resource_builder'
-  autoload :RoutesBuilder,        'azeroth/routes_builder'
-  autoload :Options,              'azeroth/options'
+  autoload :Decorator,       'azeroth/decorator'
+  autoload :DummyDecorator,  'azeroth/dummy_decorator'
+  autoload :Exception,       'azeroth/exception'
+  autoload :Model,           'azeroth/model'
+  autoload :RequestHandler,  'azeroth/request_handler'
+  autoload :Resourceable,    'azeroth/resourceable'
+  autoload :ResourceBuilder, 'azeroth/resource_builder'
+  autoload :RoutesBuilder,   'azeroth/routes_builder'
+  autoload :Options,         'azeroth/options'
 end
