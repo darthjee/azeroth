71733c141117e73d86203b8cf3b8fce59b983042
darthjee <darthjee@gmail.com>
2020-05-06 10:23:25 +0200

    Add yard doc

--- lib/azeroth/resourceable/class_methods.rb
+++ lib/azeroth/resourceable/class_methods.rb
@@ -35,6 +35,106 @@ module Azeroth
       #
       #     resource_for :document, only: %w[create index show]
       #   end
+      #
+      # @example complete example
+      #   # publishers_controller.rb
+      #
+      #   class PublishersController < ApplicationController
+      #     include Azeroth::Resourceable
+      #     skip_before_action :verify_authenticity_token
+      #
+      #     resource_for :publisher, only: %i[create index]
+      #   end
+      #
+      #   # games_controller.rb
+      #
+      #   class GamesController < ApplicationController
+      #     include Azeroth::Resourceable
+      #     skip_before_action :verify_authenticity_token
+      #
+      #     resource_for :game, except: :delete
+      #
+      #     private
+      #
+      #     def games
+      #       publisher.games
+      #     end
+      #
+      #     def publisher
+      #       @publisher ||= Publisher.find(publisher_id)
+      #     end
+      #
+      #     def publisher_id
+      #       params.require(:publisher_id)
+      #     end
+      #   end
+      #
+      #   # schema.rb
+      #
+      #   ActiveRecord::Schema.define do
+      #     self.verbose = false
+      #
+      #     create_table :publishers, force: true do |t|
+      #       t.string :name
+      #     end
+      #
+      #     create_table :games, force: true do |t|
+      #       t.string :name
+      #       t.integer :publisher_id
+      #     end
+      #    end
+      #
+      #   # publisher.rb
+      #
+      #   class Publisher < ActiveRecord::Base
+      #     has_many :games
+      #   end
+      #
+      #   # game.rb
+      #
+      #   class Game < ActiveRecord::Base
+      #     belongs_to :publisher
+      #   end
+      #
+      #   # game/decorator.rb
+      #
+      #   class Game::Decorator < Azeroth::Decorator
+      #     expose :id
+      #     expose :name
+      #     expose :publisher, decorator: NameDecorator
+      #   end
+      #
+      #   post "/publishers.json", params: {
+      #     publisher: {
+      #       name: 'Nintendo'
+      #     }
+      #   }
+      #
+      #   publisher = JSON.parse(response.body)
+      #   # returns
+      #   # {
+      #   #   'id' => 11,
+      #   #   'name' => 'Nintendo'
+      #   # }
+      #
+      #   publisher = Publisher.last
+      #   post "/publishers/#{publisher['id']}/games.json", params: {
+      #     game: {
+      #       name: 'Pokemon'
+      #     }
+      #   }
+      #
+      #   game = Game.last
+      #
+      #   JSON.parse(response.body)
+      #   # returns
+      #   # {
+      #   #   id: game.id,
+      #   #   name: 'Pokemon',
+      #   #   publisher: {
+      #   #     name: 'Nintendo'
+      #   # }
+      #   }
       def resource_for(name, **options)
         Builder.new(
           self, name, Azeroth::Options.new(options)
