c8b9462d3982d19a427c3fcdddfbe6e6a32dcd84
darthjee <darthjee@gmail.com>
2020-05-13 12:50:15 +0200

    Add docs

--- lib/azeroth/resourceable/class_methods.rb
+++ lib/azeroth/resourceable/class_methods.rb
@@ -117,6 +117,33 @@ module Azeroth
       #   #     name: 'Nintendo'
       #   # }
       #   }
+      #
+      # @example Controller with before_save
+      #   class PokemonsController < ApplicationController
+      #     include Azeroth::Resourceable
+      #
+      #     resource_for :pokemon,
+      #                  only: %i[create update],
+      #                  before_save: :set_favorite
+      #
+      #     private
+      #
+      #     def set_favorite
+      #       pokemon.favorite = true
+      #     end
+      #
+      #     def pokemons
+      #       master.pokemons
+      #     end
+      #
+      #     def master
+      #       @master ||= PokemonMaster.find(master_id)
+      #     end
+      #
+      #     def master_id
+      #       params.require(:pokemon_master_id)
+      #     end
+      #   end
       def resource_for(name, **options)
         Builder.new(
           self, name, Azeroth::Options.new(options)
