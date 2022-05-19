9940bbac977151b71124a691b65c68fd91f82ca2
darthjee <darthjee@gmail.com>
2022-05-19 11:28:58 +0200

    Update doc

--- lib/azeroth/controller_interface.rb
+++ lib/azeroth/controller_interface.rb
@@ -42,6 +42,12 @@ module Azeroth
       controller.instance_variable_set("@#{variable}", value)
     end
 
+    # Forces a controller to run a block
+    #
+    # When the block is a +Proc+ that is evaluated,
+    # when it is a +Symbol+ or +String+, a method is called
+    #
+    # @return [Object] whatever the block returns
     def run(block)
       case block
       when Proc
