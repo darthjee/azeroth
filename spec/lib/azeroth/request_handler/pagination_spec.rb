7ecf4c0ebc7d518530fb9dca1acc0f2573f56c39
darthjee <darthjee@gmail.com>
2024-05-20 18:46:02 -0300

    Rubocop fix

--- spec/lib/azeroth/request_handler/pagination_spec.rb
+++ spec/lib/azeroth/request_handler/pagination_spec.rb
@@ -10,47 +10,47 @@ describe Azeroth::RequestHandler::Pagination do
   let(:params)       { ActionController::Parameters.new(parameters) }
   let(:parameters)   { {} }
 
-  describe "offset" do
-    context "when nothing was defined" do
-      it "returns default value" do
+  describe 'offset' do
+    context 'when nothing was defined' do
+      it 'returns default value' do
         expect(pagination.offset).to be_zero
       end
     end
 
-    context "when parameters has page value" do
+    context 'when parameters has page value' do
       let(:parameters)        { { page: page } }
       let(:page)              { Random.rand(1..10) }
       let(:expected_offset)   { (page - 1) * expected_per_page }
       let(:expected_per_page) { 20 }
 
-      it "returns value from request" do
+      it 'returns value from request' do
         expect(pagination.offset).to eq(expected_offset)
       end
     end
 
-    context "when parameters has per_page value" do
+    context 'when parameters has per_page value' do
       let(:parameters)        { { per_page: per_page } }
       let(:per_page)          { Random.rand(1..10) }
       let(:expected_offset)   { 0 }
 
-      it "returns value from request" do
+      it 'returns value from request' do
         expect(pagination.offset).to eq(expected_offset)
       end
     end
 
-    context "when parameters has page and per page values" do
+    context 'when parameters has page and per page values' do
       let(:parameters)        { { page: page, per_page: per_page } }
       let(:page)              { Random.rand(1..10) }
       let(:per_page)          { Random.rand(1..10) }
       let(:expected_offset)   { (page - 1) * expected_per_page }
       let(:expected_per_page) { per_page }
 
-      it "returns value from request" do
+      it 'returns value from request' do
         expect(pagination.offset).to eq(expected_offset)
       end
     end
 
-    context "when parameters has page and options per page values" do
+    context 'when parameters has page and options per page values' do
       let(:parameters)        { { page: page } }
       let(:options_hash)      { { per_page: per_page } }
       let(:page)              { Random.rand(1..10) }
@@ -58,7 +58,7 @@ describe Azeroth::RequestHandler::Pagination do
       let(:expected_offset)   { (page - 1) * expected_per_page }
       let(:expected_per_page) { per_page }
 
-      it "returns value from request" do
+      it 'returns value from request' do
         expect(pagination.offset).to eq(expected_offset)
       end
     end
