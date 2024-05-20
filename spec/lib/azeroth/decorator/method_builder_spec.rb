f799ebc05a01cd2217edf3b164853e6314dbf514
darthjee <darthjee@gmail.com>
2024-05-20 16:58:00 -0300

    Add missing spec

--- spec/lib/azeroth/decorator/method_builder_spec.rb
+++ spec/lib/azeroth/decorator/method_builder_spec.rb
@@ -7,7 +7,8 @@ describe Azeroth::Decorator::MethodBuilder do
   let(:decorator)       { decorator_class.new(object) }
   let(:model)           { build(:dummy_model) }
   let(:object)          { model }
-  let(:options)         { Azeroth::Decorator::Options.new({}) }
+  let(:options)         { Azeroth::Decorator::Options.new(options_hash) }
+  let(:options_hash)    { {} }
 
   describe '.build_reader' do
     it do
@@ -19,6 +20,26 @@ describe Azeroth::Decorator::MethodBuilder do
       described_class.build_reader(decorator_class, :age, options)
 
       expect(decorator.age).to eq(model.age)
+    end
+
+    context "when passing reader option" do
+      context "when passing true" do
+        let(:options_hash) { { reader: true } }
+
+        it do
+          expect { described_class.build_reader(decorator_class, :age, options) }
+            .to add_method(:age).to(decorator)
+        end
+      end
+
+      context "when passing false" do
+        let(:options_hash) { { reader: false } }
+
+        it do
+          expect { described_class.build_reader(decorator_class, :age, options) }
+            .not_to add_method(:age).to(decorator)
+        end
+      end
     end
   end
 end
