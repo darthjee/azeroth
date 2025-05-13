7ee99e38b0fda7aad9ff89cba225db0b110fef8a
darthjee <darthjee@gmail.com>
2025-05-13 17:32:12 -0300

    Add spec

---
+++ spec/lib/azeroth/resourceable/resources_builder_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Azeroth::Resourceable::ResourcesBuilder do
+  subject { described_class.new(klass, :document, options) }
+
+  let(:options)      { Azeroth::Options.new(options_hash) }
+  let(:options_hash) { {} }
+
+  let(:klass) do
+    Class.new(Controller) do
+    end
+  end
+
+  describe '#build' do
+    it 'adds spec'
+  end
+end
