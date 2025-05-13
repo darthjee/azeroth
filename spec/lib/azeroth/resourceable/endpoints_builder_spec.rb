0d3f076d9cd9fe59852a6dc9c9f67793ef6c2176
darthjee <darthjee@gmail.com>
2025-05-13 18:11:47 -0300

    Add spec

--- spec/lib/azeroth/resourceable/endpoints_builder_spec.rb
+++ spec/lib/azeroth/resourceable/endpoints_builder_spec.rb
@@ -14,13 +14,23 @@ describe Azeroth::Resourceable::EndpointsBuilder do
   end
 
   describe '#build' do
-    let(:expected_resource_methods) { %i[document_id document_params] }
+    let(:expected_params_methods) { %i[document_id document_params] }
+    let(:expected_routes_methods) { %i[index show new destroy create] }
+    let(:expected_resource_methods) { %i[document documents] }
 
     it 'adds params methods' do
       expect { builder.build }
         .to change {
           methods = klass.instance_methods
-          expected_resource_methods.all? { |m| methods.include?(m) }
+          expected_params_methods.all? { |m| methods.include?(m) }
+        }
+    end
+
+    it 'adds routes methods' do
+      expect { builder.build }
+        .to change {
+          methods = klass.instance_methods
+          expected_routes_methods.all? { |m| methods.include?(m) }
         }
     end
 
